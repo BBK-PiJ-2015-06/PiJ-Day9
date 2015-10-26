88f4ab77b8d7a77a85c5e7a0ab44811e23dfa5b8
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-26 14:52:40 +0000

    First commit. Methods getMax() getMin() and toString() added and tested using TestScript.java. Methods working correctly.

---
+++ Exercise 1 - Integer Binary Tree/IntegerTreeNode.java
@@ -0,0 +1,83 @@
+public class IntegerTreeNode {
+	
+	private int value;
+	private IntegerTreeNode left;
+	private IntegerTreeNode right;
+	
+	public IntegerTreeNode(int i) {
+		this.value = i;
+		this.left = null;
+		this.right = null;
+	}
+	
+	//Method for adding new numbers to tree.
+	public void add(int newNumber) {
+		if(newNumber > this.value) {
+			if(right == null) {
+				right = new IntegerTreeNode(newNumber);
+			} else {
+				right.add(newNumber);
+			}
+		} else {
+			if(left == null) {
+				left = new IntegerTreeNode(newNumber);
+			} else {
+				left.add(newNumber);
+			}
+		}
+	}
+	
+	//Method for testing whether a number is contained within the tree.
+	public boolean contains(int number) {
+		if(number == this.value) {
+			return true;
+		} else if(number > this.value) {
+			if(right == null) {
+				return false;
+			} else {
+				return right.contains(number);
+			}
+		} else {
+			if(left == null) {
+				return false;
+			} else {
+				return left.contains(number);
+			}
+		}
+	}
+	
+	//Method for returning maximum value stored in the tree.
+	public int getMax() {
+		if(right == null) {
+			return this.value;
+		} else {
+			return right.getMax();
+		}
+	}
+	
+	//Method for returning minimum value stored in the tree.
+	public int getMin() {
+		if(left == null) {
+			return this.value;
+		} else {
+			return left.getMin();
+		}
+	}
+	
+	public String toString() {
+		String leftOutput = "";
+		String rightOutput = "";
+		if(left == null) {
+			leftOutput = "L[]";
+		} else {
+			leftOutput = "L" + left.toString();
+		}
+		if(right == null) {
+			rightOutput = "R[]";
+		} else {
+			rightOutput = "R" + right.toString();
+		}
+		String output = "[" + this.value + leftOutput + rightOutput + "]";
+		return output;
+	}
+}
---
+++ Exercise 1 - Integer Binary Tree/TestScript.java
@@ -0,0 +1,64 @@
+public class TestScript {
+	
+	public static void main(String[] args) {
+		System.out.print("Please insert first value: ");
+		String s = System.console().readLine();
+		int firstValue = Integer.parseInt(s);
+		IntegerTreeNode tree = new IntegerTreeNode(firstValue);
+		
+		boolean finished = false;
+		while(!finished) {
+			
+			System.out.println("Select option for menu below:");
+			System.out.println("1 - Add value.");
+			System.out.println("2 - Search for value.");
+			System.out.println("3 - Show maximum value.");
+			System.out.println("4 - Show minimum value.");
+			System.out.println("5 - Convert to complete string.");
+			System.out.println("Any other number - QUIT.");
+			s = System.console().readLine();
+			int choice = Integer.parseInt(s);
+			
+			switch (choice) {
+				
+				case 1:
+					System.out.print("Please insert new value to be added: ");
+					s = System.console().readLine();
+					int number = Integer.parseInt(s);
+					tree.add(number);
+					break;
+					
+				case 2:
+					System.out.print("Please insert value to search for: ");
+					s = System.console().readLine();
+					number = Integer.parseInt(s);
+					if(tree.contains(number)) {
+						System.out.println("This number is in the tree.");
+					} else {
+						System.out.println("This number is not in the tree.");
+					}
+					break;
+				
+				case 3:
+					int output = tree.getMax();
+					System.out.println("Maximum value: " + output);
+					break;
+				
+				case 4:
+					output = tree.getMin();
+					System.out.println("Minimum value: " + output);
+					break;
+				
+				case 5:
+					String complete = tree.toString();
+					System.out.println(complete);
+					break;
+				
+				default:
+					System.out.println("Exiting...");
+					finished = true;
+					break;
+			}	
+		}
+	} 	
+}
