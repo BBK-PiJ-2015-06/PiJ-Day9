ac15f27d376efa7cd0a563a7e33144e9e12823fe
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-27 10:48:48 +0000

    First commit. Interface IntSet created.

---
+++ Exercise 2 - Trees as Sets/IntSet.java
@@ -0,0 +1,25 @@
+public interface IntSet {
+	
+	/**
+	* Adds a new int to the set.
+	* If it is there already, nothing happens.
+	*/
+	void add(int i);
+	
+	/**
+	* Returns true if the number is in the set, false otherwise.
+	*/
+	boolean contains(int i);
+	
+	/**
+	* Returns the same values as the former mehtod.
+	* But for every element that is checked print its value on screen.
+	*/
+	boolean containsVerbose(int i);
+	
+	/**
+	* Returns a string with the values of the elements in the set separated by a comma.
+	*/
+	String toString();
+	
+}
